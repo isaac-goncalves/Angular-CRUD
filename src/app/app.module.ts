30246e8b922a5963b5c2329eba7be51bd7531c6c
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-03-11 11:55:46 -0300

    Added compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/template/header/header.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { NavComponent } from './component/template/nav/nav.component';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+ 
+    
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+''
